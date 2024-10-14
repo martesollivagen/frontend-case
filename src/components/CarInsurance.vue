64724005df0f23e013f9e8e63d15e0dc0b719b62
Marte Solli Vågen <martvaag@stud.ntnu.no>
2024-10-14 12:46:24 +0200

    Initial commit with current files

---
+++ src/components/CarInsurance.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div :class="['box', { 'selected': isSelected }]">
+      <div class="header">
+        <v-radio
+            :value="title"
+            :model-value="isSelected"
+            @change="$emit('update:selected', title)">
+
+          <template v-slot:label>
+            <p class="custom-radio">{{title}}</p>
+          </template>
+
+        </v-radio>
+
+        <div class="price">{{ price }} kr/mnd.</div>
+      </div>
+      <div v-if="recommended" class="recommended-tag">Anbefalt</div>
+      <div class="content">{{ description }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CarInsurance',
+  props: {
+    price: Number,
+    title: String,
+    description: String,
+    recommended: Boolean,
+    isSelected: Boolean
+  }
+}
+</script>
+
+<style scoped>
+
+.box {
+  width: 100%;
+  max-width: 400px;
+  border: 1px solid grey;
+  padding: 4px;
+  position: relative;
+}
+
+.selected {
+  border: 2px solid black;
+}
+
+.recommended-tag {
+  background-color: purple;
+  color: white;
+  font-weight: bold;
+  padding: 4px 8px;
+  border-radius: 4px;
+  position: absolute;
+  top: -20px;
+  left: -10px;
+}
+.custom-radio {
+  color: black;
+}
+
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+</style>
---
+++ src/components/CarInsuranceList.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <h3>Velg dekning:</h3>
+    <v-radio-group v-model="selected">
+      <div class="car-insurance-list">
+        <CarInsurance
+            v-for="option in insurances"
+            :key="option.title"
+            :title="option.title"
+            :price="option.price"
+            :description="option.description"
+            :recommended="option.recommended"
+            :isSelected="selected === option.title"
+            @update:selected="selected = $event"
+        />
+      </div>
+    </v-radio-group>
+    <h4 class="text-decoration-underline">Sammenlign dekninger</h4>
+  </div>
+</template>
+
+<script>
+import CarInsurance from "./CarInsurance.vue";
+
+export default {
+  name: 'CarInsuranceList',
+  components: {
+    CarInsurance
+  },
+  data() {
+    return {
+      selected: '',
+      randomComponent: null,
+      insurances: [
+        {
+          title: "Toppkasko",
+          price: 1235,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          recommended: false
+        },
+        {
+          title: "Kasko med leiebil",
+          price: 1179,
+          description: "En forsikring som passer de fleste biler. Dekker det meste av skader, også de du selv er ansvarlig for. Inkluderer: minikasko og ansvar.",
+          recommended: false
+        },
+        {
+          title: "Kasko",
+          price: 1123,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          recommended: false
+        },
+        {
+          title: "Minikasko",
+          price: 491,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          recommended: false
+        },
+        {
+          title: "Ansvar",
+          price: 362,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          recommended: false
+        },
+      ]
+    };
+  },
+  created() {
+    this.randomComponent = Math.floor(Math.random() * this.insurances.length);
+    this.insurances[this.randomComponent].recommended = true;
+  }
+}
+</script>
+
+<style scoped>
+
+.car-insurance-list {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  align-items: center;
+}
+</style>
